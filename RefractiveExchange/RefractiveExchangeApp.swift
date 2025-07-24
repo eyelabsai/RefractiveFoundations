2fa27b6d4285035bcf915bea3cbd59558ae19dd0
EyeLabsAI <gsvgff@health.missouri.edu>
2025-07-24 11:41:58 -0500

    Initial Commit

---
+++ RefractiveExchange/RefractiveExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefractiveExchangeApp.swift
+//  RefractiveExchange
+//
+//  Created by Gurpal Virdi on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct RefractiveExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
